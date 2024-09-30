8a55e732fb528f7482a198938a598a9e9fd788bc
ks6088ts <ks6088ts@gmail.com>
2024-10-01 08:49:51 +0900

    refactor code

--- sandbox_python/llms/chains/core.py
+++ sandbox_python/llms/chains/core.py
@@ -6,21 +6,23 @@ from sandbox_python.llms.core import get_chat_model
 
 llm = get_chat_model()
 
-rag_prompt = hub.pull("rlm/rag-prompt")
-rag_chain = rag_prompt | llm | StrOutputParser()
+rag_chain = hub.pull("rlm/rag-prompt") | llm | StrOutputParser()
 
-describe_image_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "Describe the image provided"),
-        (
-            "user",
-            [
-                {
-                    "type": "image_url",
-                    "image_url": {"url": "data:image/jpeg;base64,{image_data}"},
-                }
-            ],
-        ),
-    ]
+describe_image_chain = (
+    ChatPromptTemplate.from_messages(
+        [
+            ("system", "Describe the image provided"),
+            (
+                "user",
+                [
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": "data:image/jpeg;base64,{image_data}"},
+                    }
+                ],
+            ),
+        ]
+    )
+    | llm
+    | StrOutputParser()
 )
-describe_image_chain = describe_image_prompt | llm | StrOutputParser()
