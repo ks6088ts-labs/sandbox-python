58282db34a93868ba3e17785086674819118fd17
ks6088ts <ks6088ts@gmail.com>
2024-10-01 05:47:38 +0900

    support multimodal input

--- sandbox_python/llms/chains/core.py
+++ sandbox_python/llms/chains/core.py
@@ -1,9 +1,26 @@
 from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 from sandbox_python.llms.core import get_chat_model
 
 llm = get_chat_model()
-rag_prompt = hub.pull("rlm/rag-prompt")
 
+rag_prompt = hub.pull("rlm/rag-prompt")
 rag_chain = rag_prompt | llm | StrOutputParser()
+
+describe_image_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Describe the image provided"),
+        (
+            "user",
+            [
+                {
+                    "type": "image_url",
+                    "image_url": {"url": "data:image/jpeg;base64,{image_data}"},
+                }
+            ],
+        ),
+    ]
+)
+describe_image_chain = describe_image_prompt | llm | StrOutputParser()
--- sandbox_python/llms/graphs/core.py
+++ sandbox_python/llms/graphs/core.py
@@ -4,7 +4,7 @@ from typing import Any, TypedDict
 from langchain_core.documents import Document
 from langgraph.graph import END, StateGraph
 
-from sandbox_python.llms.chains.core import rag_chain
+from sandbox_python.llms.chains.core import describe_image_chain, rag_chain
 from sandbox_python.llms.core import get_embedding, get_retriever
 from sandbox_python.llms.tools.bing_search import get_bing_search_tool
 
@@ -13,6 +13,7 @@ class NodeType(Enum):
     RETRIEVE = "retrieve"
     SEARCH = "search"
     GENERATE = "generate"
+    DESCRIBE_IMAGE = "describe_image"
 
 
 class GraphState(TypedDict):
@@ -24,6 +25,7 @@ class GraphState(TypedDict):
         generation: LLM generation
         web_search: whether to add search
         documents: list of documents
+        images: list of images
         k: number of documents to retrieve
     """
 
@@ -31,6 +33,7 @@ class GraphState(TypedDict):
     generation: str
     web_search: bool
     documents: list[str]
+    images: list[str]
     k: int
 
 
@@ -85,15 +88,40 @@ def generate(state: GraphState) -> dict[str, Any]:
     return {"documents": documents, "question": question, "generation": generation}
 
 
+def describe_image(state: GraphState) -> dict[str, Any]:
+    print("---DESCRIBE_IMAGE---")
+    try:
+        images = state["images"]
+        documents = state["documents"]
+    except KeyError:
+        return state
+
+    if images is None:
+        return state
+
+    images = state["images"]
+    for image in images:
+        response = describe_image_chain.invoke({"image_data": image})
+        got_document = Document(page_content=response)
+        if documents is None:
+            documents = [got_document]
+        else:
+            documents.append(got_document)
+
+    return {"documents": documents}
+
+
 def get_graph():
     workflow = StateGraph(GraphState)
     workflow.add_node(NodeType.RETRIEVE.value, retrieve)
     workflow.add_node(NodeType.SEARCH.value, search)
+    workflow.add_node(NodeType.DESCRIBE_IMAGE.value, describe_image)
     workflow.add_node(NodeType.GENERATE.value, generate)
 
     workflow.set_entry_point(NodeType.RETRIEVE.value)
     workflow.add_edge(NodeType.RETRIEVE.value, NodeType.SEARCH.value)
-    workflow.add_edge(NodeType.SEARCH.value, NodeType.GENERATE.value)
+    workflow.add_edge(NodeType.SEARCH.value, NodeType.DESCRIBE_IMAGE.value)
+    workflow.add_edge(NodeType.DESCRIBE_IMAGE.value, NodeType.GENERATE.value)
     workflow.add_edge(NodeType.GENERATE.value, END)
 
     return workflow.compile()
